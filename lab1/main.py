d66e5ad66e7c016c8e245d7ccf84a30f0221f89d
Kirill Lapushinskiy <kirilllapushinskiy@yahoo.com>
2022-02-23 05:28:30 +0300

    New main.

--- lab1/main.py
+++ lab1/main.py
@@ -1,131 +1,92 @@
-from library import AdjacencyMatrix, RecordsArrayGraph, EdgesListGraph, AdjacencyMatrixGraph
-
-# ВАРИАНТ 10
-
-# [M]  N2  P3  E2  A3  P1  A2  N1  A1 
-#     --------------------------------
-#  N2| 0   2   0   0   0   0   0   0  
-#  P3| 0   0   4   4   34  0   0   0  
-#  E2| 5   0   0   0   5   0   0   0  
-#  A3| 9   0   0   0   0   0   0   0  
-#  P1| 0   0   0   0   0   0   6   6  
-#  A2| 0   2   0   8   0   0   0   0  
-#  N1| 0   0   0   0   0   7   0   0  
-#  A1| 0   0   0   0   0   0   0   0 
-
-ad_matrix = AdjacencyMatrix(
-    names=['N2', 'P3', 'E2', 'A3', 'P1', 'A2', 'N1', 'A1'],
-    matrix=[
-        [0, 2, 0, 0, 0, 0, 0, 0],
-        [0, 0, 4, 4, 34, 0, 0, 0],
-        [5, 0, 0, 0, 5, 0, 0, 0],
-        [9, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 6, 6],
-        [0, 2, 0, 8, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 7, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0]
-    ]
-)
-
-graph_edges = EdgesListGraph(ad_matrix)  # список рёбер
-graph_records = RecordsArrayGraph(ad_matrix)  # список записей
-graph_adjacency = AdjacencyMatrixGraph(ad_matrix)  # матрица смежности
-
-print(graph_adjacency.is_chain(['E2', 'N2', 'P3', 'P1', 'N1', 'A2']))
-print(graph_records.is_chain(['E2', 'N2', 'P3', 'P1', 'N1', 'A2']))
-print(graph_edges.is_chain(['E2', 'N2', 'P3', 'P1', 'N1', 'A2']))
-# True
-#
-print(graph_adjacency)
-# [M]  N2  P3  E2  A3  P1  A2  N1  A1
-#     --------------------------------
-#  N2| 0   2   0   0   0   0   0   0
-#  P3| 0   0   4   4   34  0   0   0
-#  E2| 5   0   0   0   5   0   0   0
-#  A3| 9   0   0   0   0   0   0   0
-#  P1| 0   0   0   0   0   0   6   6
-#  A2| 0   2   0   8   0   0   0   0
-#  N1| 0   0   0   0   0   7   0   0
-#  A1| 0   0   0   0   0   0   0   0
-
-print(graph_edges)
-# +----------+--------+
-# |   Edge   | Weight |
-# +==========+========+
-# | N2 -> P3 |   2    |
-# +----------+--------+
-# | P3 -> E2 |   4    |
-# +----------+--------+
-# | P3 -> A3 |   4    |
-# +----------+--------+
-# | P3 -> P1 |   34   |
-# +----------+--------+
-# | E2 -> N2 |   5    |
-# +----------+--------+
-# | E2 -> P1 |   5    |
-# +----------+--------+
-# | A3 -> N2 |   9    |
-# +----------+--------+
-# | P1 -> N1 |   6    |
-# +----------+--------+
-# | P1 -> A1 |   6    |
-# +----------+--------+
-# | A2 -> P3 |   2    |
-# +----------+--------+
-# | A2 -> A3 |   8    |
-# +----------+--------+
-# | N1 -> A2 |   7    |
-# +----------+--------+
+import time_test as tt
+from source import adjacency_graph, records_graph, edges_graph
 
 
-print(graph_records)
-# +---+--------+--------------+--------------------+
-# | № | Vertex |   Parents    |      Children      |
-# +===+========+==============+====================+
-# | 1 |   N2   | ['E2', 'A3'] | ['P3']             |
-# +---+--------+--------------+--------------------+
-# | 2 |   P3   | ['N2', 'A2'] | ['E2', 'A3', 'P1'] |
-# +---+--------+--------------+--------------------+
-# | 3 |   E2   |       ['P3'] | ['N2', 'P1']       |
-# +---+--------+--------------+--------------------+
-# | 4 |   A3   | ['P3', 'A2'] | ['N2']             |
-# +---+--------+--------------+--------------------+
-# | 5 |   P1   | ['P3', 'E2'] | ['N1', 'A1']       |
-# +---+--------+--------------+--------------------+
-# | 6 |   A2   |       ['N1'] | ['P3', 'A3']       |
-# +---+--------+--------------+--------------------+
-# | 7 |   N1   |       ['P1'] | ['A2']             |
-# +---+--------+--------------+--------------------+
-# | 8 |   A1   |       ['P1'] | []                 |
-# +---+--------+--------------+--------------------+
+def cli():
+    graph = None
+    test_graph = None
+    prompt1 = "Выберете граф:\n" \
+              "1 - Граф, представленный матрицей смежности.\n" \
+              "2 - Граф, представленный списком рёбер.\n" \
+              "3 - Граф, представленный списком записей\n"
 
-graph_adjacency.is_chain(['E2', 'N2', 'P3', 'P1', 'N1', 'A2'])
-# True
-graph_edges.is_chain(['E2', 'N2', 'P3', 'P1', 'N1', 'A2'])
-# True
-graph_records.is_chain(['E2', 'N2', 'P3', 'P1', 'N1', 'A2'])
-# True
+    print(prompt1)
 
-graph_adjacency.edges_number()
-# 12
-graph_edges.edges_number()
-# 12
-graph_records.edges_number()
-# 12
+    answer = input("Введите число: ")
 
-graph_adjacency.vertex_neighbors('P3')
-# ['P1', 'N2', 'E2', 'A3', 'A2']
-graph_edges.vertex_neighbors('P3')
-# ['P1', 'N2', 'E2', 'A3', 'A2']
-graph_records.vertex_neighbors('P3')
-# ['P1', 'N2', 'E2', 'A3', 'A2']
+    match answer:
+        case '1':
+            graph, test_graph = adjacency_graph, tt.AGraph
+        case '2':
+            graph, test_graph = edges_graph, tt.EGraph
+        case '3':
+            graph, test_graph = records_graph, tt.RGraph
 
-graph_adjacency.vertex_by_weights_sum(weight=36)
-# ['P3', 'P1']
-graph_edges.vertex_by_weights_sum(weight=36)
-# ['P3', 'P1']
-graph_records.vertex_by_weights_sum(weight=36)
-# ['P3', 'P1']
+    prompt2 = "Введите, что хотите сделать:\n1 - Вывод всех соседей заданной вершины.\n" \
+              "2 - Ответ, образует ли заданная последовательнсть вершин цепь.\n" \
+              "3 - Вывести номера вершин, сумма весов инцидентных ребер которых больше заданной величины.\n" \
+              "4 - Получить количество рёбер в графе.\n" \
+              "5 - Вывести размер содержашего объекта.\n" \
+              "6 - Оценить время выполнения каждого метода.\n" \
+              "(Для выхода из программы нажмите ввод.)\n"
 
-graph_edges.render()
-# PICTURE
+    print(prompt2)
+
+    while True:
+        answer = input("Введите число: ")
+        match answer:
+            case '1':
+                v = input("Введите вершину: ")
+                print(graph.vertex_neighbors(v))
+            case '2':
+                seq = input("Введите последовательность вершин через пробел: ")
+                print('Да' if graph.is_chain(seq.split()) else 'Нет')
+            case '3':
+                v = input("Введите вес: ")
+                print(graph.vertex_by_weights_sum(int(v)))
+            case '4':
+                print("Кол-во рёбер: ", graph.edges_number())
+            case '5':
+                print(f"Размер содержащего обекта {graph.size()} байт.")
+            case '6':
+                print("Оценка времени выполнения.\n")
+                t = test_graph.vertex_neighbors()
+                print(f"Поиск соседей: 10^6 раз - {t} сек. Среднее время: {t} мкс | {t * 1000} мс.")
+                t = test_graph.vertex_by_weights_sum()
+                print(f"Сумма инцидентных рёбер: 10^6 раз - {t} сек. Среднее время: {t} мкс | {t * 1000} мс.")
+                t = test_graph.edges_number()
+                print(f"Кол-во рёбер: 10^6 раз - {t} сек. Среднее время: {t} мкс | {t * 1000} мс.")
+                t = test_graph.is_chain()
+                print(f"Проверка на цепь: 10^6 раз - {t} сек. Среднее время: {t} мкс | {t * 1000} мкс.\n")
+            case _:
+                print("До свидания.")
+                break
+
+
+cli()
+
+# print(tt.EGraph.is_chain())
+# print(tt.EGraph.vertex_neighbors())
+# print(tt.EGraph.vertex_by_weights_sum())
+# print(tt.EGraph.edges_number())
+
+# edges_graph.render(show=True)
+
+# print(f"{adjacency_graph.vertex_neighbors('P3')=}")
+# print(f"{adjacency_graph.is_chain(['E2', 'N2', 'P3', 'P1', 'N1', 'A2'])=}")
+# print(f"{adjacency_graph.vertex_by_weights_sum(weight=36)=}")
+# print(f"{adjacency_graph.edges_number()=}")
+
+# print(f"{edges_graph.vertex_neighbors('P2')=}")
+# print(f"{edges_graph.is_chain(['E2', 'N2', 'P3', 'P1', 'N1', 'A1'])=}")
+# print(f"{edges_graph.vertex_by_weights_sum(weight=36)=}")
+# print(f"{edges_graph.edges_number()=}")
+
+# print(f"{edges_graph.vertex_neighbors('E2')=}")
+# print(f"{edges_graph.is_chain(['E2', 'N2', 'P3', 'P1', 'N1'])=}")
+# print(f"{edges_graph.vertex_by_weights_sum(weight=6)=}")
+# print(f"{edges_graph.edges_number()=}")
+
+# print(graph_adjacency.size())
+# print(graph_records.size())
+# print(graph_edges.size())
